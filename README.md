94fa8d69aef3832cba561b810b66df11699dcc30
Till Harbaum <till@harbaum.org>
2020-12-06 20:53:56 +0100

    Initial release

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# kvv
+# KVV
+
+We recently (Nov 2020) got a new tram extension in Karlsruhe, Germany.
+To celebrate the fact that there's now a station pretty close to our
+home I deciced to build a virtual timetable. And since I had some
+spare ESP8266 powered Wemos D1 and a 2.9" tricolor epaper display I
+decided to use these.
+
+![Breadboard setup](doc/kvv_bb.jpg)
+
+## Setup
+
+The whole setup is pretty simple and can be wired on a breadboard as
+depicted.
+
+![Wireing](doc/kvv.png)
+
+Since the epaper display keeps the data even if not powered at all the
+ESP8266 simply boots up, connects to the (hardcoded) WiFi network,
+downloads the live data, updates the display and then powers down/goes
+into deep sleep.  The whole setup can this be run from a battery with
+the reset button exposed so it can be pushed to boot the ESP8266 and 
+to update the display.
+
+## Where does the data come from?
+
+The KVV offers a [live timetable](http://live.kvv.de). A quick look
+at the network traffic (or at the embedded Javascript code) reveals that
+some easy to handle JSON can be downloaded e.g. for the tram stop "Pionierstraße" using [simple http get request](https://live.kvv.de/webapp/departures/bystop/de:0000:0003?maxInfos=10&key=377d840e54b59adbe53608ba1aad70e8).
+
+## The sketch
+
+The sketch is based upon the
+[BasicHttpsClient.ino](https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266HTTPClient/examples/BasicHttpsClient/BasicHttpsClient.ino)
+for the ESP8266. The display is driven using the [Adafruit EPD
+library](https://github.com/adafruit/Adafruit_EPD). And finally the
+JSON parsing is done using the [ArduinoJson
+library](https://github.com/bblanchon/ArduinoJson). All these
+dependencies can be installed from the library manager within the
+Arduino IDE.
+
+A custom generated font is being used since the fonts distributed by
+Adafruit with the Adafruit GFX library only includes 7 bit fonts and
+we need 8 bit fonts for the german umlauts used by the KVV.
---
+++ kvv/kvv.ino
@@ -0,0 +1,224 @@
+/*
+   kvv.ino - Till Harbaum <till@harbaum.org>
+
+   Sketch for ESP8266 driving a 2.9" tri color eink display.
+
+   Displaying the departure information for KVV tram lines in Karlsruhe.
+   Specify your preferred station id below and setup your WIFI credentials
+   and the device will update the eink display whenever powered up and go
+   to deep sleep / power save after that. Simply press reset whenever you
+   want to have the display updated. 
+*/
+
+// station ID as used at https://live.kvv.de/
+#define STOP_ID  "de:0000:0003"  // Pionierstraße
+// #define STOP_ID  "de:8212:31"    // Europaplatz
+
+#define WIFI_SSID "********"
+#define WIFI_PASSWORD "********"
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <Adafruit_ThinkInk.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <ESP8266HTTPClient.h>
+
+#include <WiFiClientSecureBearSSL.h>
+const uint8_t fingerprint[20] = {0xd2, 0x9d, 0x57, 0x4a, 0x0d, 0xab, 0x7b, 0x41, 0xd7, 0x8b, 0xd2, 0x40, 0x1b, 0x78, 0xec, 0x2c, 0x4a, 0xb6, 0xe6, 0x79};
+
+// Eink pins: DC = D0, CS=D3, BUSY=D2, SRAM_CS=-1, RESET=D1
+ThinkInk_290_Tricolor_Z10 display(D0, D1, D3, -1, D2);
+
+ESP8266WiFiMulti WiFiMulti;
+
+#define TOP 15
+#define SKIP 18
+#define COL0_WIDTH 28
+#include "FreeSansBold9pt8b.h"
+
+// ****** UTF8-Decoder: convert UTF8-string to extended ASCII *******
+
+// Convert a single Character from UTF8 to Extended ASCII
+// Return "0" if a byte has to be ignored
+byte utf8ascii(byte ascii) {
+  static byte c1;  // Last character buffer
+  
+  if ( ascii<128 ) {  // Standard ASCII-set 0..0x7F handling  
+    c1=0;
+    return( ascii );
+  }
+
+  // get previous input
+  byte last = c1;   // get last char
+  c1=ascii;         // remember actual character
+
+  switch (last) {  // conversion depending on first UTF8-character
+    case 0xC2: return  (ascii);  break;
+    case 0xC3: return  (ascii | 0xC0);  break;
+    case 0x82: if(ascii==0xAC) return(0x80);       // special case Euro-symbol
+  }
+
+  return  (0);                                     // otherwise: return zero, if character has to be ignored
+}
+
+// convert String object from UTF8 String to Extended ASCII
+String utf8ascii(String s) {      
+  String r="";
+  char c;
+  for (int i=0; i<s.length(); i++) {
+    c = utf8ascii(s.charAt(i));
+    if (c!=0) r+=c;
+  }
+  return r;
+}
+
+void parse_reply(String payload) {
+  int16_t  x, y;
+  uint16_t w, h;
+  
+  DynamicJsonDocument doc(2048);
+          
+  // start parsing
+  DeserializationError error = deserializeJson(doc, payload); 
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+
+  display.begin(THINKINK_TRICOLOR);
+  display.setFont(&FreeSansBold9pt8b);
+  display.clearBuffer();
+  display.setTextSize(1);
+
+  JsonObject obj = doc.as<JsonObject>();
+
+  String stopName = obj[String("stopName")];
+  String timeStamp = obj[String("timestamp")];
+
+  // reformat time string from YYYY-MM-DD HH:MM:SS to a narrower
+  // (D)D.(M)M.YY (H)H:MM
+  timeStamp = timeStamp.substring((timeStamp.charAt(8)=='0')?9:8,10) + "." +
+              timeStamp.substring((timeStamp.charAt(5)=='0')?6:5,7) + "." +
+              timeStamp.substring(2,4) + " " +
+              timeStamp.substring((timeStamp.charAt(11)=='0')?12:11,16);
+
+  // top row has a black background
+  display.fillRect(0, 0, 296, SKIP, EPD_BLACK);
+
+  // station name in white
+  display.setTextColor(EPD_WHITE);
+  display.setCursor(COL0_WIDTH+2, TOP);
+  display.print(utf8ascii(stopName));
+
+  // time stamp is right aligned
+  display.getTextBounds(timeStamp, 0, 0, &x, &y, &w, &h);
+  display.setTextColor(EPD_WHITE);
+  display.setCursor(295-w-2, TOP);
+  display.print(timeStamp);
+  
+  for(int i=0;i<obj[String("departures")].size();i++) {  
+    JsonObject nobj = obj[String("departures")][i];
+    String route = nobj[String("route")];
+    String destination = nobj[String("destination")];
+    String time = nobj[String("time")];
+    if(time == "0") time = "sofort";
+
+    // other (yet unused) fields:
+    // traction: integer, number of carriages. 0=1 carriage, 1=1, 2=2, ...
+    // lowfloor: true, use wheelchair icon
+    // realtime: so far, only "true" has been observed
+    // direction: always 1 or 2 for forth and back
+
+    // up to 6  entries fit onto the screen
+    if(i < 6) {
+      int16_t  x, y;
+      uint16_t w, h;
+
+      // route column has red background
+      display.fillRect(0, TOP+SKIP*i+5, COL0_WIDTH, SKIP-2, EPD_RED);
+
+      // center route in first column
+      display.setTextColor(EPD_WHITE);
+      display.getTextBounds(route, 0, 0, &x, &y, &w, &h);
+      display.setCursor((COL0_WIDTH-w)/2-x, TOP+SKIP+SKIP*i);
+      display.print(route);
+
+      // left align destination
+      display.setTextColor(EPD_BLACK, EPD_WHITE);
+      display.setCursor(COL0_WIDTH+2, TOP+SKIP+SKIP*i);
+      display.print(utf8ascii(destination));
+
+      // display time right aligned
+      display.getTextBounds(time, 0, 0, &x, &y, &w, &h);
+      display.setTextColor(EPD_RED, EPD_WHITE);
+      display.setCursor(295-w-2, TOP+SKIP+SKIP*i);
+      display.print(time);
+    }
+  }  
+  display.display();  
+}
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+
+  // wait for WiFi link up
+  while((WiFiMulti.run() != WL_CONNECTED)) {
+    Serial.print('.');
+    digitalWrite(LED_BUILTIN, HIGH);  // led off
+    delay(50);
+    digitalWrite(LED_BUILTIN, LOW);   // led on
+    delay(50);
+  }
+
+  std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+
+  client->setFingerprint(fingerprint);
+
+  HTTPClient https;
+
+  Serial.print("[HTTPS] begin...\n");
+  if (https.begin(*client, "https://live.kvv.de/webapp/departures/bystop/" STOP_ID
+      "?maxInfos=10"
+      "&key=377d840e54b59adbe53608ba1aad70e8")) {  // HTTPS
+
+    Serial.print("[HTTPS] GET...");
+    // start connection and send HTTP header
+    int httpCode = https.GET();
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf(" code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY)
+        parse_reply(https.getString());
+    } else
+      Serial.printf(" failed, error: %s\n", https.errorToString(httpCode).c_str());
+
+    https.end();
+  } else
+    Serial.printf("[HTTPS] Unable to connect\n");
+    
+  Serial.println("Going to sleep ...");
+  ESP.deepSleep(0); 
+}
+
+void loop() {
+  Serial.printf("loop() should never be reached!\n");
+  delay(1000);
+}
